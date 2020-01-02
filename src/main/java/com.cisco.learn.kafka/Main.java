26f9d46d61cd3bb8e7a4d53e260c977c552a7ea9
songlli <songlli@cisco.com>
2020-01-02 16:07:34 +0800

    Initial Commit

---
+++ src/main/java/com.cisco.learn.kafka/Main.java
@@ -0,0 +1,30 @@
+package com.cisco.learn.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+
+import java.util.Properties;
+
+/**
+ * @author songlli
+ * @since 2019-12-31 16:41
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "10.124.113.187:9092");
+        properties.put("acks", "all");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(properties);
+        for (int i = 0; i < 100; i++) {
+            producer.send(new ProducerRecord<String, String>("my-topic", Integer.toBinaryString(i), Integer.toString(i)));
+        }
+
+        producer.close();
+    }
+}
