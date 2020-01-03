c58fb8f3850e664d42a2a28bb6350776fb9a7c20
songlli <xiaopengyoumencaicaiwoshishui@gmail.com>
2020-01-03 13:43:02 +0800

    Use ProducerConfig instead of string

--- src/main/java/com.cisco.learn.kafka/Main.java
+++ src/main/java/com.cisco.learn.kafka/Main.java
@@ -2,7 +2,9 @@ package com.cisco.learn.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 
 import java.util.Properties;
@@ -15,14 +17,15 @@ public class Main {
 
     public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.put("bootstrap.servers", "10.124.113.187:9092");
-        properties.put("acks", "all");
-        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.124.113.187:9092");
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
 
         Producer<String, String> producer = new KafkaProducer<String, String>(properties);
         for (int i = 0; i < 100; i++) {
-            producer.send(new ProducerRecord<String, String>("my-topic", Integer.toBinaryString(i), Integer.toString(i)));
+            producer.send(new ProducerRecord<String, String>("my-topic-test", Integer.toBinaryString(i), Integer.toString(i)));
         }
 
 
