05c8337f48d9fbf059cd8661b0484495702b7033
songlli <xiaopengyoumencaicaiwoshishui@gmail.com>
2020-01-04 23:51:58 +0800

    Use ProducerConfig instead of string

--- src/main/java/com.cisco.learn.kafka/Main.java
+++ src/main/java/com.cisco.learn.kafka/Main.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 
@@ -19,15 +20,16 @@ public class Main {
         Properties properties = new Properties();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.124.113.187:9092");
         properties.put(ProducerConfig.ACKS_CONFIG, "all");
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,  LongSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-
         Producer<String, String> producer = new KafkaProducer<String, String>(properties);
-        for (int i = 0; i < 100; i++) {
-            producer.send(new ProducerRecord<String, String>("my-topic-test", Integer.toBinaryString(i), Integer.toString(i)));
-        }
-
+//        for (int i = 0; i < 100; i++) {
+//            producer.send(new ProducerRecord<String, String>("my-topic-test", Integer.toBinaryString(i), Integer.toString(i)));
+//        }
+//        long time = System.currentTimeMillis();
+//        for (long index = time; index < time; index++) {
+//            -
+//        }
 
         producer.close();
     }
